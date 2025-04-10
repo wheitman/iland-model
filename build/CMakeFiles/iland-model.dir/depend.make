99a9088a64f4ea592069f86760d490152c3f72ea
Will Heitman <will@heit.mn>
2025-04-10 19:52:21 -0400

    Apply .gitignore retroactively

--- build/CMakeFiles/iland-model.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for iland-model.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/iland-model_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/iland-model_autogen"
-  "iland-model_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/iland-model_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
